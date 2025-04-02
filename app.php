94945cec538229991dbf3829f83809d0671c9d36
Shubham03007 <56829263+Shubham03007@users.noreply.github.com>
2025-04-02 17:04:48 +0530

    Update and rename app.html to app.php

--- app.php
+++ app.php
@@ -1,10 +1,13 @@
+<?php
+header("Content-Type: text/html");
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Hacked!</title>
-<style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hacked!</title>
+    <style>
         body {
             background-color: black;
             color: red;
@@ -13,9 +16,9 @@
             text-align: center;
             margin-top: 20%;
         }
-</style>
+    </style>
 </head>
 <body>
-    You are hacked Bachhi!
+    <?php echo "You are hacked!"; ?>
 </body>
 </html>
